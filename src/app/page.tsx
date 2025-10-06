c8fcad7834f00ce855b7ffb3087680440f001ba9
Aman Khanna <91634727+aman-X-code@users.noreply.github.com>
2025-10-07 01:31:51 +0530

    Updated page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,5 @@
-"use client";
-
-import { HeroSection } from "@/components/Hero";
-import { ServicesSection } from "@/components/ServicesSection";
-import { AboutSection } from "@/components/AboutSection";
-import { PortfolioSection } from "@/components/PortfolioSection";
-import { ContactSection } from "@/components/ContactSection";
-import ClickSpark from "@/components/ClickSpark";
-import ClientOnly from "@/components/ClientOnly";
+import HomeClient from "@/components/HomeClient";
 
 export default function Home() {
-  return (
-    <div suppressHydrationWarning>
-      <ClientOnly>
-        <ClickSpark
-          sparkColor='#00d4ff'
-          sparkSize={10}
-          sparkRadius={15}
-          sparkCount={8}
-          duration={400}
-        >
-          <main suppressHydrationWarning>
-            <HeroSection />
-            <ServicesSection />
-            <PortfolioSection />
-            <AboutSection />
-            <ContactSection />
-          </main>
-        </ClickSpark>
-      </ClientOnly>
-    </div>
-  );
+  return <HomeClient />;
 }
