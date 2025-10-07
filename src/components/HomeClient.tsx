9fbc51a8be6e2c72431a7aecd96bb0137a1e6b4a
Aman Khanna <91634727+aman-X-code@users.noreply.github.com>
2025-10-07 23:16:21 +0530

    Updated HomeClient.tsx

--- src/components/HomeClient.tsx
+++ src/components/HomeClient.tsx
@@ -5,29 +5,15 @@ import { ServicesSection } from "@/components/ServicesSection";
 import { AboutSection } from "@/components/AboutSection";
 import { PortfolioSection } from "@/components/PortfolioSection";
 import { ContactSection } from "@/components/ContactSection";
-import ClickSpark from "@/components/ClickSpark";
-import ClientOnly from "@/components/ClientOnly";
 
 export default function HomeClient() {
   return (
-    <div suppressHydrationWarning>
-      <ClientOnly>
-        <ClickSpark
-          sparkColor='#00d4ff'
-          sparkSize={10}
-          sparkRadius={15}
-          sparkCount={8}
-          duration={400}
-        >
-          <main suppressHydrationWarning>
-            <HeroSection />
-            <ServicesSection />
-            <PortfolioSection />
-            <AboutSection />
-            <ContactSection />
-          </main>
-        </ClickSpark>
-      </ClientOnly>
-    </div>
+    <main suppressHydrationWarning>
+      <HeroSection />
+      <ServicesSection />
+      <PortfolioSection />
+      <AboutSection />
+      <ContactSection />
+    </main>
   );
 }
